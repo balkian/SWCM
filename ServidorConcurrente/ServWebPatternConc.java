ad903e0124e25540891726ebba8bfa67c6f0406b
Fernando Sánchez <balkian@gmail.com>
2011-04-24 20:50:42 +0200

    Java version with simple thread management added

---
+++ ServidorConcurrente/ServWebPatternConc.java
@@ -0,0 +1,33 @@
+package ServidorConcurrente;
+
+import java.util.*;   
+import java.io.*;   
+import java.net.*;
+
+public class ServWebPatternConc { 
+  public static int maxCon = 10;
+  public static void main (String args[]) {        
+    try {
+      ServerSocket serv = new ServerSocket(8080);
+      System.out.println("showserver created at port 8080.");
+	ThreadGroup workers = new ThreadGroup("WebWorkers");
+
+      while (true) {
+	if(workers.activeCount()<maxCon){
+  	 System.out.println("Hay "+workers.activeCount()+" hebras corriendo.");
+         Socket conn = serv.accept();
+	 Thread hs = new Thread(workers, new HebraServ(conn));
+	 hs.start();
+	}
+	else{
+	 try{
+	 	Thread.sleep(500);
+	 } 
+	 catch(Exception ex){	
+	 }
+	}
+      }
+    } 
+    catch (IOException e) { System.err.println(e); }
+  }
+}
